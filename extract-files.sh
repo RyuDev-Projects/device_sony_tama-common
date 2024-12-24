25a3ba8244452f1c2bf23cde1f975d15bb0fd8b0
Bruno Martins <bgcngm@gmail.com>
2024-12-24 09:33:33 +0000

    tama-common: Update WFD system stack from OOS 15.0.0.201(EX01)
    Change-Id: Ia31624004a10c5e471a146646237bd240e38d07a
    Signed-off-by: KanonifyX <kanonify01@gmail.com>
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -76,14 +76,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s/\/product\/framework\//\/system_ext\/framework\//g" "${2}"
             ;;
-        system_ext/lib/libwfdmmsrc_system.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
-            ;;
-        system_ext/lib64/libwfdnative.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
-            ;;
         system_ext/lib64/lib-imsvideocodec.so)
             [ "$2" = "" ] && return 0
             grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
