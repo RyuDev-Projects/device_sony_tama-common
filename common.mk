c286a16f33f0072cbe448527a75d28ba9f585d3a
romiyusnandar <yusromi04@gmail.com>
2025-02-23 12:58:49 +0000

    tama-common: We use AOSP NFC
    This reverts commit bccedf51b77bbbd852d399d136cfd6f3bcbbb8d4.
    
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- common.mk
+++ common.mk
@@ -328,6 +328,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     libchrome.vendor \
@@ -336,9 +337,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/init.tama.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.tama.nfc.rc
 
 # Power
 PRODUCT_PACKAGES += \
